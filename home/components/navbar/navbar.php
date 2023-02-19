afeebbdb833311363f8c69aeec49196a638379c0
Den4ikJLoJL <93444321+Den4ikJLoJL@users.noreply.github.com>
2023-02-19 22:16:10 +0200

    feat || edit links in navbar html

--- home/components/navbar/navbar.php
+++ home/components/navbar/navbar.php
@@ -2,9 +2,9 @@
     <div class="wrapper">
         <div class="logo">PCShop</div>
         <div class="link-container">
-            <a href="./index.php?source=productsCatalog">Pc Catalog</a>
-            <a href="./index.php?source=addProduct">Add Pc</a>
-            <a href="./index.php?source=activeOrders">Active Orders</a>
+            <a href="./index.php?source=productsCatalog">Home</a>
+            <a href="./index.php?source=addProduct">Shop</a>
+            <a href="./index.php?source=activeOrders">About us</a>
         </div>
     </div>
 </section>
