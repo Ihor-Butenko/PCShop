7fe0dfc15551e1bc470c0c8be947cfaf8c79c022
Ihor Butenko <91276970+Ihor-Butenko@users.noreply.github.com>
2023-02-19 22:40:45 +0200

    feat || add css

--- admin/components/navbar/navbar.php
+++ admin/components/navbar/navbar.php
@@ -1,7 +1,7 @@
 <section class="navbar">
     <div class="wrapper">
-        <div class="logo">PCShop | Admin</div>
-        <div><h1>Welcome back, user👋</h1></div>
+        <div class="logo">PCShop Admin</div>
+        <div class="welcome"><h1>Welcome back, user👋</h1></div>
         <div class="link-container">
             <a href="./index.php?source=productsCatalog">Pc Catalog</a>
             <a href="./index.php?source=addProduct">Add Pc</a>
