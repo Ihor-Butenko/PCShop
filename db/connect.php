d4d50f21e6b972151bd536c1d6597ae5c13352de
Ihor Butenko <91276970+Ihor-Butenko@users.noreply.github.com>
2023-02-19 20:34:34 +0200

    feat || add connect to database & add product sql

---
+++ db/connect.php
@@ -0,0 +1,15 @@
+<!-- db connect -->
+
+<?php
+
+    $dbAdress = "localhost";
+    $dbUsername = "admin";
+    $dbPassword = "admin";
+    $dbName = "PCShop";
+
+    $connect = new mysql($dbAdress, $dbUsername, $dbPassword, $dbName);
+
+    // Username: admin
+    // Password: admin
+
+?>
---
+++ db/sql/products.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Фев 19 2023 г., 19:13
+-- Версия сервера: 10.4.24-MariaDB
+-- Версия PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `pcshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(80) NOT NULL,
+  `product_name` varchar(90) NOT NULL,
+  `product_price` varchar(40) NOT NULL,
+  `product_image` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(80) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
