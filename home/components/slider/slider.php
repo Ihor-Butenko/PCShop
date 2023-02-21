2f4d3234a831281f8292b5e1998e98857f0b7141
Den4ikJLoJL <93444321+Den4ikJLoJL@users.noreply.github.com>
2023-02-21 23:47:54 +0200

    feat || add image in slider

--- home/components/slider/slider.php
+++ home/components/slider/slider.php
@@ -1,6 +1,6 @@
 <section class="slider">
-    <div class="slider_left_container">
     <div class="slider_wrapper">
+    <div class="slider_left_container">
         <div class="slider_text_container">
             <a class="slider_text">Last RTX-TI Graphic Card</a>
         </div>
@@ -8,5 +8,10 @@
             <input type="button" value="SHOP NOW">
         </div>
     </div>
+    
+
+        <div class="slider_right_container">
+            <img src="/slider/images/graphics_card.png" alt="qwdawd">
+        </div>
     </div>
 </section>
