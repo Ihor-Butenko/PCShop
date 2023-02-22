e6ce327ae234ff3423c0fd6df21d7466a479f5d8
Den4ikJLoJL <93444321+Den4ikJLoJL@users.noreply.github.com>
2023-02-22 20:04:57 +0200

    feat || add image

--- home/components/slider/slider.php
+++ home/components/slider/slider.php
@@ -1,6 +1,7 @@
 <section class="slider">
-    <div class="slider_left_container">
     <div class="slider_wrapper">
+    <div class="slider_left_container">
+    
         <div class="slider_text_container">
             <a class="slider_text">Last RTX-TI Graphic Card</a>
         </div>
@@ -8,5 +9,8 @@
             <input type="button" value="SHOP NOW">
         </div>
     </div>
+        <div class="slider_right_container">
+            <img src="./images/graphics_card.png" alt="">
+        </div>
     </div>
 </section>
