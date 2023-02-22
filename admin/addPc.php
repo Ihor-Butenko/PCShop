747dd35aa584f4fe03f32b00039020065e46dcc3
Den4ikJLoJL <93444321+Den4ikJLoJL@users.noreply.github.com>
2023-02-22 21:36:24 +0200

    feat || add div for add Pc

--- admin/addPc.php
+++ admin/addPc.php
@@ -2,6 +2,11 @@
 
 <div class="popup" id="mypopup">
     <div class="wrapper">
+            <div class="addPc_top_container">
+                <div class="addPc_top_text_container">
+                    ADD PC
+                </div>
+            </div>
             
         </div>
     </div>
