303d9cbf6f6063e35e83d140b33d6470e76ce939
Ihor Butenko <91276970+Ihor-Butenko@users.noreply.github.com>
2023-02-23 09:44:16 +0200

    feat || add html layot for popup windwod

--- admin/catalog.php
+++ admin/catalog.php
@@ -32,9 +32,17 @@
 
             <?php }?>
 
-
-
-
+            <div class="popup" id="mypopup">
+                <div class="wrapper">
+                        <div class="addPc_top_container">
+                            <div class="addPc_top_text_container">
+                                ADD PC
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
 
         </div>
     </div>
