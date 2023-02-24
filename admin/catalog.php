9b4f8fa43bcd3b1df1ef90cb59ce62540f6613e5
Ihor Butenko <91276970+Ihor-Butenko@users.noreply.github.com>
2023-02-24 22:04:01 +0200

    feat || complete frontend admin product card section & add change price dynamic data

--- admin/catalog.php
+++ admin/catalog.php
@@ -23,14 +23,44 @@
                 <img src="../image/<?php echo $productImage;?>">
                 <h1><?php echo $productName;?></h1>
                 <div class="price-container">
-                    <p><?php echo $productPrice;?></p>
+                    <p><?php echo $productPrice . "$";?></p>
                     <div class="price-button">
-                        <a>Change Price</a>
+                        <a class="price-text">Change Price</a>
                     </div>
                 </div>
+                <div class="button-container">
+                    <div class="button" id="left"><a>View All</a></div>
+                    <div class="button" id="right"><a>Edit Product</a></div>
+                </div>
+            <?php }?>
+                <form method="post">
+                    <div class="input-container">
+                        <input name="new-price" type="varchar" placeholder="000">
+                        <button type="submit" name="submit-price">Submit</button>
+
+                        <?php
+
+                            if(isset($_POST["submit-price"])){
+
+                                $newPrice = $_POST["new-price"];
+
+                                $updatePriceSQL = "UPDATE products SET product_price = $newPrice";
+                                $updateQuery = $connect -> query($updatePriceSQL);
+
+                                if(isset($updateQuery)){
+                                    header("Location: index.php?source=productsCatalog");
+                                }
+
+                            }                        
+                        
+                        ?>
+                    
+
+                    </div>
+                </form>
+
             </div>
 
-            <?php }?>
 
             <div class="popup" id="mypopup">
                 <div class="wrapper">
--- admin/components/footer.php
+++ admin/components/footer.php
@@ -1,4 +1,5 @@
 <!-- JS CODE HERE -->
 <script src="./js/popup.js"></script>
+<script src="./js/catalog.js"></script>
 </body>
 </html>
