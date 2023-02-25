f8d22991efa310912a84f1aa1616a4dd08fda267
Ruma445 <126194873+Ruma445@users.noreply.github.com>
2023-02-25 17:22:28 +0200

    add || featured products

--- home/components/featured_products/featured_products.php
+++ home/components/featured_products/featured_products.php
@@ -2,56 +2,81 @@
 
     <div><h1>Featured Products</h1></div>
 
+
     <div>
+
         <div>
-            
-            <p>PREMIUM AUDIO</p>
+            <div>
+                
+                <p>PREMIUM AUDIO</p>
 
+            </div>
+            <div>
+
+                <i class="fa-regular fa-heart"></i>
+                <i class="fa-solid fa-arrow-right-arrow-left"></i>
+
+
+            </div>
         </div>
-        <div></div>
-    </div>
 
-    <div>
         <div>
-            
-            <p>PREMIUM AUDIO</p>
+            <div>
+                
+                <p>PREMIUM AUDIO</p>
 
+            </div>
+            <div></div>
         </div>
-        <div></div>
-    </div>
 
-    <div>
         <div>
+            <div>
 
-            <p>PREMIUM AUDIO</p>
+                <p>PREMIUM AUDIO</p>
 
+            </div>
+            <div></div>
         </div>
-        <div></div>
-    </div>
 
-    <div>
         <div>
+            <div>
 
-            <p>PREMIUM AUDIO</p>
+                <p>PREMIUM AUDIO</p>
 
+            </div>
+            <div></div>
         </div>
-        <div></div>
-    </div>
 
-    <div>
-        <div></div>
-        <div></div>
-    </div>
+        <div>
+            <div>
 
-    <div>
-        <div></div>
-        <div></div>
-    </div>
+                <p>PREMIUM AUDIO</p>
 
-    <div>
-        <div></div>
-        <div></div>
-    </div>
+            </div>
+            <div></div>
+        </div>
 
+        <div>
+            <div>
+
+                <p>PREMIUM AUDIO</p>
+
+            </div>
+            <div></div>
+        </div>
+
+        <div>
+            <div>
+
+                <p>PREMIUM AUDIO</p>    
+
+            </div>
+            <div></div>
+        </div>
+
+
+    </div>
 
 </div>
+
+    
