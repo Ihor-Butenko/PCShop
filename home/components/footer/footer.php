3a25313744c2e73f498c2097be25a15645b87199
K1Robus <onopriuchukkirill@gmail.com>
2023-02-25 15:16:52 +0200

    feat | | add lists

--- home/components/footer/footer.php
+++ home/components/footer/footer.php
@@ -7,18 +7,64 @@
 		<input type="text" placeholder="Your Email Address">
 
 		<div class="top_footer_icon_container">
+
 			<i class="fa-brands fa-spotify"></i>
-			
+			<i class="fa-brands fa-youtube"></i>
+			<i class="fa-brands fa-tiktok"></i>
+			<i class="fa-brands fa-twitter"></i>
+			<i class="fa-brands fa-instagram"></i>
+			<i class="fa-brands fa-facebook"></i>
+			<i class="fa-brands fa-discord"></i>
+
 		</div>
+	<div class="lists_footer_container">
+		<div>
+			<ul>Privacy Policy
+				<li><a>Returns & Exchanges</a></li>
+				<li><a>Payment Terms</a></li>
+				<li><a>Delivery Terms</a></li>
+				<li><a>Payment & Pricing</a></li>
+				<li><a>Terms Of Use</a></li>
+				<li><a>Privacy Policy</a></li>
+			</ul>
 
+		</div>
+		<div>
 
-	</div class="lists_footer_container">
+			<ul>Get Involved
+				<li><a>About Us</a></li>
+				<li><a>Our Vision</a></li>
+				<li><a>Orders & Shipping</a></li>
+				<li><a>Office Supplies</a></li>
+				<li><a>Contact Us</a></li>
+				<li><a>Customer Service</a></li>
+			</ul>
 
+		</div>
+		<div>
 
-	
-	<div>
+			<ul>Quick Links
+				<li><a>Smartphones</a></li>
+				<li><a>Headphones</a></li>
+				<li><a>Laptop & Tablet</a></li>
+				<li><a>Monitors</a></li>
+				<li><a>Printers</a></li>
+				<li><a>Gadgets</a></li>
+			</ul>
 
+		</div>
+		<div>
+			<ul>Customer Care
+				<li><a>My Account</a></li>
+				<li><a>Store Locator</a></li>
+				<li><a>Customer Service</a></li>
+				<li><a>Returns/Exchange</a></li>
+				<li><a>Product Support</a></li>
+				<li><a>FAQs</a></li>
+			</ul>
+		</div>
 	</div>
+
 
 	<div>
 
