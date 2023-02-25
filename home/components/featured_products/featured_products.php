1dd59f986f2acd820fe17cfed3946386e953f162
Ruma445 <126194873+Ruma445@users.noreply.github.com>
2023-02-25 17:10:13 +0200

    feat || add feature products

---
+++ home/components/featured_products/featured_products.php
@@ -0,0 +1,57 @@
+<div>
+
+    <div><h1>Featured Products</h1></div>
+
+    <div>
+        <div>
+            
+            <p>PREMIUM AUDIO</p>
+
+        </div>
+        <div></div>
+    </div>
+
+    <div>
+        <div>
+            
+            <p>PREMIUM AUDIO</p>
+
+        </div>
+        <div></div>
+    </div>
+
+    <div>
+        <div>
+
+            <p>PREMIUM AUDIO</p>
+
+        </div>
+        <div></div>
+    </div>
+
+    <div>
+        <div>
+
+            <p>PREMIUM AUDIO</p>
+
+        </div>
+        <div></div>
+    </div>
+
+    <div>
+        <div></div>
+        <div></div>
+    </div>
+
+    <div>
+        <div></div>
+        <div></div>
+    </div>
+
+    <div>
+        <div></div>
+        <div></div>
+    </div>
+
+
+</div>
